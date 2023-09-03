333441ffbfc544ede986e529ebd5ac8d05adc9be
DenisZaviryuhin <denzava30@gmail.com>
2023-09-03 22:24:01 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,31 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int a = 0;
+        for (int i = 1; i <= 6; i++) {
+            a++;
+            sum += i;
+            System.out.println(a + ") Num is " + i + "," + " sum is " + sum);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
